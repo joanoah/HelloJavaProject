ffc29c769c06e07e1e095a0374105f4bd1790cb7
joanoah <joanoah0613@gmail.com>
2024-02-21 16:51:30 +0300

    Added a new line in IntelliJ

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main (String[] args) {
         System.out.println("Hello git and git hub");
+        System.out.println("This is a new line added in IntelliJ");
     }
 }
